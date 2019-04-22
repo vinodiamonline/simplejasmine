824b384da6085462b7e647b632d3a0fe07f849bd
vinkumar <vinkumar@adobe.com>
2019-04-22 14:58:04 +0530

    optimized calc

--- src/calculator.js
+++ src/calculator.js
@@ -1,15 +1,7 @@
 function add(x,y) {
 	function ParseArg(n) {
-		if(Array.isArray(n)) {
-			var result = 0;
-			for (var index = 0, count=n.length; index<count; index++) 
-				result += ParseArg(n[index]);
-			return result;
-		}
+		if(Array.isArray(n)) return add.apply(this, n);
 		return (typeof n === "function") ? ParseArg(n()) : (isNaN(n) ? 0 : parseInt(n));
 	}
-	var result = 0;
-	for(var index = 0, count = arguments.length; index < count; index++)
-		result += ParseArg(arguments[index]);
-	return result;
+	return arguments.length <= 1 ? ParseArg(arguments[0]) : ParseArg(arguments[0]) + add([].slice.call(arguments,1));
 }
